f01d27bb829c6358f16d76e5dcca7099efb72d48
Alex Chong <alex1107@gmail.com>
2018-12-22 23:07:07 -0500

    added enzyme and setupTests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow, mount } from "enzyme";
 import App from "./app";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App />", () => {
+  it("renders without crashing", () => {
+    //const div = document.createElement("div");
+    //ReactDOM.render(<App />, div);
+    //ReactDOM.unmountComponentAtNode(div);
+    shallow(<App />);
+  });
 });
