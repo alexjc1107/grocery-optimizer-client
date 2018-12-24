7f4a7be8a36eb8c44250c442686fcf8c04768d81
Alex Chong <alex1107@gmail.com>
2018-12-23 20:07:35 -0500

    try fix travis test error

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { shallow, mount } from "enzyme";
-import App from "./app";
+import App from "./app.js";
 import "../setupTest";
 
 describe("<App />", () => {
